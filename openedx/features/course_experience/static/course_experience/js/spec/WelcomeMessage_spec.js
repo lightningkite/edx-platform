2d0709ef58b690dae00e47aa46907acd606029d6
Diana Huang <dkh@edx.org>
2017-06-19 09:11:45 -0400

    Address quality issues.

--- openedx/features/course_experience/static/course_experience/js/spec/WelcomeMessage_spec.js
+++ openedx/features/course_experience/static/course_experience/js/spec/WelcomeMessage_spec.js
@@ -1,23 +1,23 @@
 /* globals $, loadFixtures */
 
-import { WelcomeMessage } from '../WelcomeMessage';
 import { AjaxHelpers } from 'edx-ui-toolkit/js/utils/spec-helpers/ajax-helpers';
+import { WelcomeMessage } from '../WelcomeMessage';
 
 describe('Welcome Message factory', () => {
   describe('Ensure button click', () => {
     const endpointUrl = '/course/course_id/dismiss_message/';
     beforeEach(() => {
       loadFixtures('course_experience/fixtures/welcome-message-fragment.html');
-      new WelcomeMessage(endpointUrl);
+      WelcomeMessage(endpointUrl);
     });
     it('When button click is made, ajax call is made and message is hidden.', () => {
       const message = document.querySelector('.welcome-message');
       document.querySelector('.dismiss-message button').dispatchEvent(new Event('click'));
-      requests = AjaxHelpers.requests(this);
+      const requests = AjaxHelpers.requests(this);
       AjaxHelpers.expectRequest(
         requests,
         'POST',
-        endpointUrl
+        endpointUrl,
       );
       AjaxHelpers.respondWithJson(requests);
       expect(message.innerHtml).toBe('');
